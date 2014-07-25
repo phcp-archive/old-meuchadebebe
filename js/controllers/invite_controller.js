ff19ce432028b64ac3da3db0534d25f70a825584
PauloHenrique <paulohenrique@Saber2891.(none)>
2014-07-25 14:44:21 -0300

    Correcoes

--- js/controllers/invite_controller.js
+++ js/controllers/invite_controller.js
@@ -1,9 +1,4 @@
 $(function() {
-  Parse.$ = jQuery;
-
-  Parse.initialize("iD91Kruwny1uP1UNRCspAGNHSLHINUkEGuhe2N6E",
-   "jcCjFbysCwxymP6dfpBC4QE2ch75S3Y7xwevEvHo");
-
   var AppState = Parse.Object.extend("AppState");
 
   var AppView = Parse.View.extend({
---
+++ js/controllers/parse_controller.js
@@ -0,0 +1,6 @@
+$(function() {
+  Parse.$ = jQuery;
+
+  Parse.initialize("iD91Kruwny1uP1UNRCspAGNHSLHINUkEGuhe2N6E",
+   "jcCjFbysCwxymP6dfpBC4QE2ch75S3Y7xwevEvHo");
+});
