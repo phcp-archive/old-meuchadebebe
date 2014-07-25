8e920fa24a2049e8c95db52949b27c259c66be39
PauloHenrique <paulohenrique@Saber2891.(none)>
2014-07-25 14:48:56 -0300

    Correcoes 2

--- js/views/moms_module/login_view.js
+++ js/views/moms_module/login_view.js
@@ -1,50 +1,48 @@
-$(function() {
-  var LogInView = Parse.View.extend({
-    events: {
-      "submit form.login-form": "logIn",
-    },
+var LogInView = Parse.View.extend({
+  events: {
+    "submit form.login-form": "logIn",
+  },
 
-    el: "#full-content",
-    
-    initialize: function() {
-      _.bindAll(this, "logIn");
-      this.render();
-    },
+  el: "#full-content",
+  
+  initialize: function() {
+    _.bindAll(this, "logIn");
+    this.render();
+  },
 
-    logIn: function(e) {
-      var self = this;
+  logIn: function(e) {
+    var self = this;
 
-      Parse.FacebookUtils.logIn("email", {
-        success: function(user) {
-          FB.api(
-            "/me?fields=name,picture",
-            function (response) {
-              if (response && !response.error) {
-                Parse.User.current().save({"name": response.name});
-                Parse.User.current().save({"picture": response.picture.data.url});
+    Parse.FacebookUtils.logIn("email", {
+      success: function(user) {
+        FB.api(
+          "/me?fields=name,picture",
+          function (response) {
+            if (response && !response.error) {
+              Parse.User.current().save({"name": response.name});
+              Parse.User.current().save({"picture": response.picture.data.url});
 
-                self.$(".login-form button").attr("disabled", "disabled");
-                self.$el.html("");
-                new MenuView();
-                new MomsView();
-                self.undelegateEvents();
-                delete self;
-              }
+              self.$(".login-form button").attr("disabled", "disabled");
+              self.$el.html("");
+              new MenuView();
+              new MomsView();
+              self.undelegateEvents();
+              delete self;
             }
-          );
-        },
-        error: function(user, error) {
-          self.$("#error").html("Problemas ao logar com o Facebook, tente novamente.").show();
-          self.$(".login-form button").removeAttr("disabled");
-        }
-      });
+          }
+        );
+      },
+      error: function(user, error) {
+        self.$("#error").html("Problemas ao logar com o Facebook, tente novamente.").show();
+        self.$(".login-form button").removeAttr("disabled");
+      }
+    });
 
-      return false;
-    },
+    return false;
+  },
 
-    render: function() {
-      this.$el.html(_.template($("#login-template").html()));
-      this.delegateEvents();
-    }
-  });
+  render: function() {
+    this.$el.html(_.template($("#login-template").html()));
+    this.delegateEvents();
+  }
 });
