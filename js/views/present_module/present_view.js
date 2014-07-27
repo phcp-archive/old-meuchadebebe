6d535f8277f271ee0c6c8c22b8bfe1f0cdddf3f7
CynthiaRaphaella <cynthia_raphaella02@hotmail.com>
2014-07-26 22:50:44 -0300

    Presente

---
+++ js/views/present_module/present_view.js
@@ -0,0 +1,37 @@
+var PresenteView = Parse.View.extend({
+  events: {
+    "submit form.presente-form": "save",
+  },
+
+  el: "#content",
+
+  initialize: function() {
+    _.bindAll(this, "save");
+
+    this.render();
+  },
+
+  save: function() {
+    var nomedopresente = this.$("#event-nomedopresente").val();
+    var quantidade = this.$("#event-quantidadedopresente").val();
+
+    var evt = new Presente();
+    evt.set("nome", nomedopresente);
+
+    evt.save(null, {
+      success: function(evento) {
+        new MomsView();
+        self.undelegateEvents();
+        delete self;
+      },
+      error: function(error) {
+        this.$("#error").html("Problemas ao salvar dados no servidor, aguarde e tente novamente.").show();
+      }
+    });
+  },
+
+  render: function() {
+    this.$el.html(_.template($("#presente-template").html()));
+    this.delegateEvents();
+  }
+});
